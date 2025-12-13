1eab7518d0eb2e9abf07690b2ff94004de62fcee
berni <bernizapico@gmail.com>
2025-12-13 15:02:09 -0300

    feat: added base structure Field

--- src/main/java/ar/edu/itba/sds/engine/Field.java
+++ src/main/java/ar/edu/itba/sds/engine/Field.java
@@ -1,4 +1,40 @@
 package ar.edu.itba.sds.engine;
 
-public class Field {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Field implements Iterable<Time>{
+    private List<Zombie> zombies;
+    private List<Civilian> civilians;
+    private List<Doctor> doctors;
+    private List<Agent> allAgents;
+    private double borderRadius;
+
+    public Field(double borderRadius) {
+        this.zombies = new ArrayList<>();
+        this.civilians = new ArrayList<>();
+        this.doctors = new ArrayList<>();
+        this.borderRadius = borderRadius;
+    }
+
+    public void addZombie(Zombie zombie) {
+        zombies.add(zombie);
+        allAgents.add(zombie);
+    }
+
+    public void addCivilian(Civilian civilian) {
+        civilians.add(civilian);
+        allAgents.add(civilian);
+    }
+
+    public void addDoctor(Doctor doctor) {
+        doctors.add(doctor);
+        allAgents.add(doctor);
+    }
+
+    @Override
+    public Iterator<Time> iterator() {
+        return null;
+    }
 }
---
+++ src/main/java/ar/edu/itba/sds/engine/Time.java
@@ -0,0 +1,6 @@
+package ar.edu.itba.sds.engine;
+
+import java.util.List;
+
+public record Time(double time, List<Agent> particles) {
+}
