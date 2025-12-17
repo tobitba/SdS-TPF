b2f036dfef19be54af51f710fb75b21f2ede4037
tobias <tpugliano@itba.edu.ar>
2025-12-17 10:19:06 -0300

    feat: add PostProcessor

---
+++ src/main/java/ar/edu/itba/sds/tools/PostProcessor.java
@@ -0,0 +1,53 @@
+package ar.edu.itba.sds.tools;
+
+import ar.edu.itba.sds.engine.Agent;
+
+import java.io.BufferedWriter;
+import java.io.Closeable;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PostProcessor implements Closeable {
+    private static final String OUTPUT_FILE_NAME = "dynamicOutput.txt";
+    private final BufferedWriter writer;
+
+    public PostProcessor(String outputName) {
+        try {
+            if (outputName == null)
+                outputName = OUTPUT_FILE_NAME;
+            writer = new BufferedWriter(new FileWriter(outputName));
+        } catch (IOException e) {
+            throw new RuntimeException("Error opening file");
+        }
+    }
+
+    public void processEpoch(Time time) {
+        try {
+            writer.write(String.valueOf(time.time()));
+            writer.newLine();
+            time.civilians().forEach(this::processParticle);
+            writer.newLine();
+            time.doctors().forEach(this::processParticle);
+            writer.newLine();
+            time.zombies().forEach(this::processParticle);
+            writer.newLine();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing on output file");
+        }
+    }
+
+    private void processParticle(Agent agent) {
+        try {
+            writer.write(agent.toString());
+            writer.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing on output file");
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        writer.close();
+    }
+}
