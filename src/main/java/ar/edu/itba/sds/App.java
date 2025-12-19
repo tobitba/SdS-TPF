80ceb9c69685becee5c75a07942cc537423d703b
berni <bernizapico@gmail.com>
2025-12-19 01:04:04 -0300

    fix: forgot to add output_name as a program argument

--- src/main/java/ar/edu/itba/sds/App.java
+++ src/main/java/ar/edu/itba/sds/App.java
@@ -11,15 +11,17 @@ public class App {
     private static final String CIVILIAN_COUNT = "hc";
     private static final String DOCTOR_COUNT = "hd";
     private static final String MAX_TIME = "duration";
+    private static final String OUTPUT = "output";
 
     public static void main(String[] args)  {
         int civilianCount = Integer.parseInt(System.getProperty(CIVILIAN_COUNT));
         int doctorCount = Integer.parseInt(System.getProperty(DOCTOR_COUNT));
         double maxTime = Double.parseDouble(System.getProperty(MAX_TIME));
+        String output = System.getProperty(OUTPUT);
 
         Field field = new Field(11, civilianCount, doctorCount, maxTime);
         Iterator<Time> timeIterator = field.iterator();
-        try(PostProcessor postProcessor = new PostProcessor("dynamicOutput.txt")) {
+        try(PostProcessor postProcessor = new PostProcessor(output)) {
             postProcessor.processEpoch(field.getCurrentTime()); //t = 0
             timeIterator.forEachRemaining(postProcessor::processEpoch);
         }catch (IOException e) {
